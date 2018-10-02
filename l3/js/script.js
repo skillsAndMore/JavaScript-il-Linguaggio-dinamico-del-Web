9d396e29c01550afb0c924d7edcee33af4b27530
Andrea Barghigiani <andrea@codeat.it>
2018-10-02 11:24:59 +0200

    fix l3

--- l3/js/script.js
+++ l3/js/script.js
@@ -1,1 +1,20 @@
-alert("Ciao da SkillsAndMore");
+/*
+	All'interno della lezione abbiamo parlato delle variabili che possono contenere i seguenti tipi di valori:
+	- numeri
+	- stringhe
+	- booleani
+	- nulli
+	- array
+	- oggetti
+*/
+
+//Dichiaro più variabili senza assegnare valore
+var pippo, paperino, pluto;
+
+//Dichiaro più variabili assegnando un valore
+var nome = "Andrea",
+	cognome = "Barghigiani";
+
+// Dichiaro variabili che contengono stringhe e mostro come usare i caratteri di escape
+var singoli =	'Questa è una stringa con singoli apici e per usarli al suo interno devo usare un carattere di 'escape'.';
+var doppi =	"Questa è una stringa con doppi apici e posso usare 'i singoli' senza \"escape\", ma talvolta è necessario.";
